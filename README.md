12bae00c7ef4547bd2c14dbc97ccbc3032b5a031
DKNorad <nedd.petrov@gmail.com>
2022-05-03 23:40:54 +0300

    Added a GUI

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Cisco Router IOS/IOS-XE Web Scraper
-
+![GUI](https://i.imgur.com/fa4eLdd.png)
 ### Description:  
 A web scraper that collects the following information about the Cisco suggested releases for the platform chosen.  
 - Version
@@ -20,6 +20,3 @@ The script is currently working with the following routers:
     - ASR1001-RP1, ASR1002-RP1, ASR1004-RP1, ASR1006-RP1
     - ASR1004-RP2, ASR1006-RP2, ASR1006-X-RP2, ASR1009-X-RP2, ASR1012-RP2
     - ASR1006-X-RP3, ASR1009-X-RP3, ASR1012-RP3
-
-
-TODO: Include a GUI with drop-down menus for more convenient use.
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from tkinter import *
 import time
 
 IOS_VERSIONS = {'ISR1905': '282977117', 'ISR1906C': '283035751', 'ISR1921': '282977114', 'ISR1941': '282774238',
@@ -18,6 +19,8 @@ IOSXE_VERSIONS = {'ISR4221': '286310700', 'ISR4321': '286006221', 'ISR4331': '28
                   'ASR1006-RP2': '282450665', 'ASR1006-X-RP2': '282450665', 'ASR1009-X-RP2': '282450665',
                   'ASR1012-RP2': '282450665', 'ASR1006-X-RP3': '286308009', 'ASR1009-X-RP3': '286308009',
                   'ASR1012-RP3': '286308009'}
+outputs1 = {}
+outputs2 = {}
 
 
 class WebPage:
@@ -28,36 +31,14 @@ class WebPage:
         self.options.add_argument('log-level=3')
         self.driver = webdriver.Chrome("chromedriver.exe", options=self.options)
 
-        self.IOS = '280805680'
-        self.IOS_XE = '282046477'
-        self.plat_type = ''
-        self.plat = ''
-        self.platform = ''
-        self.outputs1 = {}
-        self.outputs2 = {}
         self.suggested_versions = []
         self.ios_url = ''
 
-    def get_platform(self):
-        # Choose platform and check if it is valid.
-        self.platform = input('Please input device(example: ASR1001-RP1, ISR4431, ISR2911): ').upper()
-        while True:
-            if self.platform in IOS_VERSIONS:
-                self.plat = 'IOS'
-                self.plat_type = IOS_VERSIONS.get(self.platform)
-                break
-            elif self.platform in IOSXE_VERSIONS:
-                self.plat = 'IOS-XE'
-                self.plat_type = IOSXE_VERSIONS.get(self.platform)
-                break
-            print(f'{self.platform} is not a valid device.')
-            self.platform = input('Please input device(example: ASR1001-RP1, ISR4431, ISR2911): ').upper()
-
-    def get_suggested(self):
-        if self.plat == 'IOS':
-            self.ios_url = f'https://software.cisco.com/download/home/{self.plat_type}/type/{self.IOS}/release/'
-        elif self.plat == 'IOS-XE':
-            self.ios_url = f'https://software.cisco.com/download/home/{self.plat_type}/type/{self.IOS_XE}/release/'
+    def get_suggested(self, model, ios_type):
+        if model in IOS_VERSIONS:
+            self.ios_url = f'https://software.cisco.com/download/home/{IOS_VERSIONS[model]}/type/{ios_type}/release/'
+        else:
+            self.ios_url = f'https://software.cisco.com/download/home/{IOSXE_VERSIONS[model]}/type/{ios_type}/release/'
 
         # Open url to the chosen device.
         self.driver.get(self.ios_url)
@@ -72,9 +53,8 @@ class WebPage:
             if el != 'Suggested Release':
                 self.suggested_versions.append(el.replace('(MD)', '').replace('(ED)', ''))
 
-    def get_info(self):
-        self.get_platform()
-        self.get_suggested()
+    def get_info(self, model, ios_type):
+        self.get_suggested(model, ios_type)
 
         count = 1
         for ver in self.suggested_versions:
@@ -108,43 +88,87 @@ class WebPage:
 
             # Add the values as a list entry
             if count == 1:
-                self.outputs1['version'] = version.text
-                self.outputs1['bin_name'] = bin_name.text
-                self.outputs1['release_date'] = release_date.text
-                self.outputs1['md5'] = md5.text
-                self.outputs1['release_notes'] = release_notes.get_attribute("href")
-                self.outputs1['download_url'] = self.ios_url + ver
+                outputs1['version'] = version.text
+                outputs1['bin_name'] = bin_name.text
+                outputs1['release_date'] = release_date.text
+                outputs1['md5'] = md5.text
+                outputs1['release_notes'] = release_notes.get_attribute("href")
+                outputs1['download_url'] = self.ios_url + ver
             else:
-                self.outputs2['version'] = version.text
-                self.outputs2['bin_name'] = bin_name.text
-                self.outputs2['release_date'] = release_date.text
-                self.outputs2['md5'] = md5.text
-                self.outputs2['release_notes'] = release_notes.get_attribute("href")
-                self.outputs2['download_url'] = self.ios_url + ver
+                outputs2['version'] = version.text
+                outputs2['bin_name'] = bin_name.text
+                outputs2['release_date'] = release_date.text
+                outputs2['md5'] = md5.text
+                outputs2['release_notes'] = release_notes.get_attribute("href")
+                outputs2['download_url'] = self.ios_url + ver
+                write_text(model)
             count += 1
 
     def __repr__(self):
         pass
 
+    # function for closing the web driver
     def quit(self):
         self.driver.quit()
 
 
+def write_text(model):
+    output.delete('1.0', END)
+    output.insert('end', f'Cisco suggested releases for the {model} are:\n'
+                         f'Version: {outputs1.get("version")}\n'
+                         f'File name: {outputs1.get("bin_name")}\n'
+                         f'Release date: {outputs1.get("release_date")}\n'
+                         f'MD5: {outputs1.get("md5")}\n'
+                         f'Release notes: {outputs1.get("release_notes")}\n'
+                         f'Download link: {outputs1.get("download_url")}\n\n\n\n'
+                         f'Version: {outputs2.get("version")}\n'
+                         f'File name: {outputs2.get("bin_name")}\n'
+                         f'Release date: {outputs2.get("release_date")}\n'
+                         f'MD5: {outputs2.get("md5")}\n'
+                         f'Release notes: {outputs2.get("release_notes")}\n'
+                         f'Download link: {outputs2.get("download_url")}\n')
+
+
+# Function for closing the tkinter window
+def close_window():
+    window.destroy()
+
+
 web = WebPage()
-web.get_info()
+
+# Defining Tkinter parameters
+window = Tk()
+window.title('Cisco IOS/IOS-XE Web Scraper')
+window.geometry("1100x400")
+window.config(background="white")
+
+# Create buttons, labels and a text box
+ios_label = Label(window, text='IOS platforms')
+iosxe_label = Label(window, text='IOS-XE platforms')
+output = Text(height=320, width=250, wrap='word')
+
+dropdown1 = StringVar(window)
+dropdown1.set("ISR1905")  # default value
+dd1 = OptionMenu(window, dropdown1, *IOS_VERSIONS)
+
+dropdown2 = StringVar(window)
+dropdown2.set("ISR4221")  # default value
+dd2 = OptionMenu(window, dropdown2, *IOSXE_VERSIONS)
+
+ios_confirm = Button(window, text="Extract", command=lambda: web.get_info(dropdown1.get(), 280805680), width=10)
+iosxe_confirm = Button(window, text="Extract", command=lambda: web.get_info(dropdown2.get(), 282046477), width=10)
+button_exit = Button(window, text="Exit", command=close_window, width=10)
+
+# Place the elements in the window
+ios_label.place(x=10, y=10)
+iosxe_label.place(x=10, y=70)
+ios_confirm.place(x=140, y=32)
+iosxe_confirm.place(x=140, y=92)
+dd1.place(x=10, y=32)
+dd2.place(x=10, y=92)
+output.place(x=100, y=125)
+button_exit.place(x=10, y=370)
+
+# Let the windows wait for any commands
+window.mainloop()
 web.quit()
-
-print(f'Cisco suggested releases for the {web.platform} are:')
-print(f'Version: {web.outputs1.get("version")}\n'
-      f'File name: {web.outputs1.get("bin_name")}\n'
-      f'Release date: {web.outputs1.get("release_date")}\n'
-      f'MD5: {web.outputs1.get("md5")}\n'
-      f'Release notes: {web.outputs1.get("release_notes")}\n'
-      f'Download link: {web.outputs1.get("download_url")}\n\n')
-
-print(f'Version: {web.outputs2.get("version")}\n'
-      f'File name: {web.outputs2.get("bin_name")}\n'
-      f'Release date: {web.outputs2.get("release_date")}\n'
-      f'MD5: {web.outputs2.get("md5")}\n'
-      f'Release notes: {web.outputs2.get("release_notes")}\n'
-      f'Download link: {web.outputs2.get("download_url")}\n')
